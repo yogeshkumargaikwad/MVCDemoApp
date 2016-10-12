fe0fb4e663025a217b49ae1d9591d3c433debcb2
exit <yogeshkumar2711@gmail.com>
2016-10-12 19:44:35 +0530

    This is first code commit

---
+++ MVCDemoApp/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace MVCDemoApp.App_Start
+{
+	public class BundleConfig
+	{
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			bundles.Add(new ScriptBundle("~/bundles/angular").IncludeDirectory("~/vendor_js","*.js"));
+			bundles.Add(new StyleBundle("~/Content/css").IncludeDirectory("~/vendor_css",".css"));
+		}
+	}	
+}
